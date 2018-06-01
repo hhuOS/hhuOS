802faf718c4b19d0aa09a18d732a6b3199f7b438
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2018-06-01 10:49:40 +0200

    Document OutputStream

--- src/os/lib/OutputStream.cc
+++ src/os/lib/OutputStream.cc
@@ -1,27 +1,9 @@
-/*****************************************************************************
- *                                                                           *
- *                           O U T S T R E A M                               *
- *                                                                           *
- *---------------------------------------------------------------------------*
- * Beschreibung:    Die Klasse OutputStream enthaelt die Definition des      *
- *                  << Operators fuer die wichtigsten der vordefinierten     *
- *                  Datentypen und realisiert somit die bekannte Ausgabe-    *
- *                  funktion der C++ iO_Stream Bibliothek. Zur Zeit wird     *
- *                  die Darstellung von Zeichen, Zeichenketten und ganzen    *
- *                  Zahlen unterstuetzt. Ein weiterer << Operator erlaubt    *
- *                  die Verwendung von Manipulatoren.                        *
- *                                                                           *
- *                  Neben der Klasse OutputStream sind hier auch die         *
- *                  Manipulatoren hex, dec, oct und bin fuer die Wahl der    *
- *                  Basis bei der Zahlendarstellung, sowie endl fuer den     *
- *                  Zeilenumbruch definiert.                                 *
- *                                                                           *
- * Autor:           Olaf Spinczyk, TU Dortmund                               *
- *                  Aenderungen von Michael Schoettner, HHU, 1.8.16          *
- *****************************************************************************/
-
 #include "lib/OutputStream.h"
 
+
+OutputStream::OutputStream() {
+    base = 10;
+}
 
 void OutputStream::writeBytes(char *data, uint64_t len) {
     for(uint64_t i = 0; i < len; i++) {
@@ -29,9 +11,6 @@ void OutputStream::writeBytes(char *data, uint64_t len) {
     }
 }
 
-//
-// Zeichen und Zeichenketten in Stream ausgeben
-//
 OutputStream& OutputStream::operator << (char c) {
     put(c);
     return *this;
@@ -54,10 +33,6 @@ OutputStream& OutputStream::operator << (const char* string) {
     return *this << (char *) string;
 }
 
-//
-//  Ganzer Zahlen im Zahlensystem zur Basis base in Stream ausgeveb
-//  Alle vorzeichenbehafteten Datentypen werden als long dargestellt,
-//  Alle vorzeichenlosen als unsigned long.
 OutputStream& OutputStream::operator << (short ival) {
     return *this << (long) ival;
 }
@@ -74,46 +49,39 @@ OutputStream& OutputStream::operator << (unsigned int ival) {
     return *this << (unsigned long) ival;
 }
 
-// Darstellung eine vorzeichenbehafteten ganzen Zahl.
 OutputStream& OutputStream::operator << (long ival) {
-    // Bei negativen Werten wird ein Minuszeichen ausgegeben.
     if (ival < 0) {
-        put ('-');
+        put('-');
         ival = -ival;
     }
-    // Dann wird der Absolutwert als vorzeichenlose Zahl ausgegeben.
+
     return *this << (unsigned long) ival;
 }
 
-// Darstellung einer vorzeichenlosen ganzen Zahl.
 OutputStream& OutputStream::operator << (unsigned long ival) {
     unsigned long div;
     char digit;
 
     if (base == 8)
-        put ('0');         // oktale Zahlen erhalten eine fuehrende Null
+        put ('0');
     else if (base == 16) {
-        put ('0');         // hexadezimale Zahlen ein "0x"
+        put ('0');
         put ('x');
     }
 
-    // Bestimmung der groessten Potenz der gewaehlten Zahlenbasis, die
-    // noch kleiner als die darzustellende Zahl ist.
     for (div = 1; ival/div >= (unsigned long) base; div *= base);
 
-    // ziffernweise Ausgabe der Zahl
     for (; div > 0; div /= (unsigned long) base) {
-        digit = ival / div;
+        digit = static_cast<char>(ival / div);
         if (digit < 10)
                 put ('0' + digit);
         else
-                put ('A' + digit - 10);
+                put (static_cast<char>('A' + digit - 10));
         ival %= div;
     }
     return *this;
 }
 
-// Darstellung eines Zeigers als hexadezimale ganze Zahl
 OutputStream& OutputStream::operator << (void* ptr) {
     int oldbase = base;
     base = 16;
@@ -122,47 +90,31 @@ OutputStream& OutputStream::operator << (void* ptr) {
     return *this;
  }
 
-//   Aufruf einer Manipulatorfunktion
 OutputStream& OutputStream::operator << (OutputStream& (*f) (OutputStream&)) {
     return f(*this);
 }
 
-//
-// Manipulatorfunktionen
-//
-// Die folgenden Funktionen erhalten und liefern jeweils eine Referenz auf
-// ein OutputStream Objekt. Da die Klasse O_Stream einen Operator << fuer
-// derartige Funktionen definiert, koennen sie mit Hilfe dieses Operators
-// aufgerufen und sogar in weitere Eingaben eingebettet werden.
-// Aufgabe der Manipulatoren ist, die Darstellung der nachfolgenden Ausgaben
-// zu beeinflussen, z.B durch die Wahl des Zahlensystems.
-
-// Fuege einen Zeilenumbruch in die Ausgabe ein.
 OutputStream& endl (OutputStream& os) {
     os << '\n';
     os.flush ();
     return os;
 }
 
-// Waehlt das binaere Zahlensystem aus.
 OutputStream& bin (OutputStream& os) {
     os.base = 2;
     return os;
 }
 
-// Waehlt das oktale Zahlensystem aus.
 OutputStream& oct (OutputStream& os) {
     os.base = 8;
     return os;
 }
 
-// Waehlt das dezimale Zahlensystem aus.
 OutputStream& dec (OutputStream& os) {
     os.base = 10;
     return os;
 }
 
-// Waehlt das hexadezimale Zahlensystem aus.
 OutputStream& hex (OutputStream& os) {
     os.base = 16;
     return os;
--- src/os/lib/OutputStream.h
+++ src/os/lib/OutputStream.h
@@ -1,95 +1,177 @@
-/*****************************************************************************
- *                                                                           *
- *                           O U T S T R E A M                               *
- *                                                                           *
- *---------------------------------------------------------------------------*
- * Beschreibung:    Die Klasse OutputStream enthaelt die Definition des      *
- *                  << Operators fuer die wichtigsten der vordefinierten     *
- *                  Datentypen und realisiert somit die bekannte Ausgabe-    *
- *                  funktion der C++ iO_Stream Bibliothek. Zur Zeit wird     *
- *                  die Darstellung von Zeichen, Zeichenketten und ganzen    *
- *                  Zahlen unterstuetzt. Ein weiterer << Operator erlaubt    *
- *                  die Verwendung von Manipulatoren.                        *
- *                                                                           *
- *                  Neben der Klasse OutputStream sind hier auch die         *
- *                  Manipulatoren hex, dec, oct und bin fuer die Wahl der    *
- *                  Basis bei der Zahlendarstellung, sowie endl fuer den     *
- *                  Zeilenumbruch definiert.                                 *
- *                                                                           *
- * Autor:           Olaf Spinczyk, TU Dortmund                               *
- *                  Aenderungen von Michael Schoettner, HHU, 1.8.16          *
- *****************************************************************************/
+/*
+ * Copyright (C) 2018 Burak Akguel, Christian Gesse, Fabian Ruhland, Filip Krakowski, Michael Schoettner
+ * Heinrich-Heine University
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 #ifndef __OutputStream_include__
 #define __OutputStream_include__
 
 #include "lib/StringBuffer.h"
 
+/**
+ * Contains definitions for the C++ output-stream operator.
+ * An implementation only needs to implement flush() for the operator to work.
+ *
+ * @author Olaf Spinczyk, TU Dortmund; Michael Schoettner, Fabian Ruhland, HHU
+ * @date 2017
+ */
 class OutputStream : public StringBuffer {
 
 private:
-      OutputStream(const OutputStream &copy); // Verhindere Kopieren
+    int base;
 
 public:
-      int base;   // Basis des Zahlensystems: z.B. 2, 8, 10 oder 16
+    /**
+    * Constructor.
+    */
+    OutputStream ();
 
-      OutputStream () : StringBuffer () { base = 10; }   // initial Dezimalsystem
-      virtual ~OutputStream () {}
+    /**
+     * Copy-constructor.
+     */
+    OutputStream(const OutputStream &copy) = delete;
 
-      virtual void flush () = 0;                    // weiterhin undefiniert             Zeichenkette.
+    /**
+     * Destructor.
+     */
+    ~OutputStream() override = default;
 
-      void writeBytes(char *data, uint64_t len);
+    /**
+     * Flushes the output buffer.
+     * An implementation needs to write StringBuffer::buffer to its destination.
+     */
+    void flush() override = 0;
 
-      // OPERATOR << : Umwandlung des angegebenen Datentypes in eine
-      //
+    /**
+     * Write binary data to the output buffer.
+     * This method won't write until it encounters a \0, as the stream operators do. Instead it writes as much bytes
+     * as are specified by len, regardless of their value.
+     *
+     * @param data The data to be written.
+     * @param len The amount of bytes to be written.
+     */
+    void writeBytes(char *data, uint64_t len);
 
-      // Darstellung eines Zeichens (trivial)
-      OutputStream& operator << (char c);
-      OutputStream& operator << (unsigned char c);
+    /**
+     * Write a char.
+     *
+     * @param c The char.
+     */
+    OutputStream& operator << (char c);
 
-      // Darstellung einer nullterminierten Zeichenkette
-      OutputStream& operator << (char* string);
-      OutputStream& operator << (const char* string);
+    /**
+     * Write a char.
+     *
+     * @param c The char.
+     */
+    OutputStream& operator << (unsigned char c);
 
-      //  Darstellung ganzer Zahlen im Zahlensystem zur Basis base
-      OutputStream& operator << (short ival);
-      OutputStream& operator << (unsigned short ival);
-      OutputStream& operator << (int ival);
-      OutputStream& operator << (unsigned int ival);
-      OutputStream& operator << (long ival);
-      OutputStream& operator << (unsigned long ival);
+    /**
+     * Write a null-terminated string.
+     * @param string The string.
+     */
+    OutputStream& operator << (char* string);
 
-      // Darstellung eines Zeigers als hexadezimale ganze Zahl
-      OutputStream& operator << (void* ptr);
+    /**
+     * Write a null-terminated string.
+     * @param string The string.
+     */
+    OutputStream& operator << (const char* string);
 
-      // Aufruf einer Manipulatorfunktion
-      OutputStream& operator << (OutputStream& (*f) (OutputStream&));
+    /**
+     * Write a short integer and display it according to the current base.
+     *
+     * @param ival The integer.
+     */
+    OutputStream& operator << (short ival);
+
+    /**
+     * Write an unsigned short integer and display it according to the current base.
+     *
+     * @param ival The integer.
+     */
+    OutputStream& operator << (unsigned short ival);
+
+    /**
+     * Write an integer and display it according to the current base.
+     *
+     * @param ival The integer.
+     */
+    OutputStream& operator << (int ival);
+
+    /**
+     * Write an unsigned integer and display it according to the current base.
+     *
+     * @param ival The integer.
+     */
+    OutputStream& operator << (unsigned int ival);
+
+    /**
+     * Write a long integer and display it according to the current base.
+     *
+     * @param ival The integer.
+     */
+    OutputStream& operator << (long ival);
+
+    /**
+     * Write an unsigned long integer and display it according to the current base.
+     *
+     * @param ival The integer.
+     */
+    OutputStream& operator << (unsigned long ival);
+
+    /**
+     * Write a pointer as hexadecimal nuber.
+     *
+     * @param ptr The pointer
+     */
+    OutputStream& operator << (void* ptr);
+
+    /**
+     * Manipulate the stream using the manipulator functions.
+     */
+    OutputStream& operator << (OutputStream& (*f) (OutputStream&));
 
 };
 
-
-//
-// Manipulatorfunktionen
-//
-// Die folgenden Funktionen erhalten und liefern jeweils eine Referenz auf
-// ein OutputStream Objekt. Da die Klasse OutputStream einen Operator << fuer
-// derartige Funktionen definiert, koennen sie mit Hilfe dieses Operators
-// aufgerufen und sogar in weitere Eingaben eingebettet werden.
-// Aufgabe der Manipulatoren ist, die Darstellung der nachfolgenden Ausgaben
-// zu beeinflussen, z.B durch die Wahl des Zahlensystems.
-
-// Zeilenumbruch in Ausgabe einfuegen.
+/**
+ * Line break.
+ */
 OutputStream& endl (OutputStream& os);
 
-// Waehle binaeres Zahlensystem aus.
+/**
+ * Change the OutputStream's base to 2.
+ * All numbers will then be displayed in binary notation.
+ */
 OutputStream& bin (OutputStream& os);
 
-// Waehle oktales Zahlensystem aus.
+/**
+ * Change the OutputStream's base to 8.
+ * All numbers will then be displayed in octal notation.
+ */
 OutputStream& oct (OutputStream& os);
 
-// Waehle dezimales Zahlensystem aus.
+/**
+ * Change the OutputStream's base to 10.
+ * All numbers will then be displayed in decimal notation.
+ */
 OutputStream& dec (OutputStream& os);
 
-// Waehle hexadezimales Zahlensystem aus.
+/**
+ * Change the OutputStream's base to 16.
+ * All numbers will then be displayed in hexadecimal notation.
+ */
 OutputStream& hex (OutputStream& os);
 
 #endif
