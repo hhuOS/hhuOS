a4877687e820bcc4dd19c2ca86dbb2c47233f656
Filip Krakowski <Filip.Krakowski@Uni-Duesseldorf.de>
2018-07-11 21:55:54 +0200

    Fix SSE memory alignment

--- src/os/apps/Mandelbrot/Mandelbrot.cc
+++ src/os/apps/Mandelbrot/Mandelbrot.cc
@@ -7,152 +7,87 @@
 
 #include <xmmintrin.h>
 
-struct spec {
-    /* Image Specification */
-    int width;
-    int height;
-    int depth;
-    /* Fractal Specification */
-    float xlim[2];
-    float ylim[2];
-    int iterations;
-};
-
-typedef __m128 __m128a __attribute__ ((aligned (16)));
-
-struct MandelbrotState {
-    __m128 xmin;
-    __m128 ymin;
-    __m128 xscale;
-    __m128 yscale;
-    __m128 threshold;
-    __m128 one;
-    __m128 iter_scale;
-    __m128 depth_scale;
-    __m128 mx;
-    __m128 my;
-    __m128 cr;
-    __m128 ci;
-    __m128 zr;
-    __m128 zi;
-    __m128 mk;
-    __m128 zr2;
-    __m128 zi2;
-    __m128 zrzi;
-    __m128 mag2;
-    __m128 mask;
-    __m128i pixels;
-} ;
-
-MandelbrotState mandelbrotState;
-
-void mandel_sse2(unsigned char *image, const struct spec *s) {
-
-    Cpu::disableInterrupts();
-
-    mandelbrotState.xmin = _mm_set_ps1(1.0F);
-    mandelbrotState.ymin = _mm_set_ps1(2.0F);
-
-    __m128a r = _mm_mul_ps(mandelbrotState.xmin, mandelbrotState.ymin);
-
-//    mandelbrotState.xmin = _mm_set_ps1(s->xlim[0]);
-//    mandelbrotState.ymin = _mm_set_ps1(s->ylim[0]);
-//    mandelbrotState.xscale = _mm_set_ps1((s->xlim[1] - s->xlim[0]) / s->width);
-//    mandelbrotState.yscale = _mm_set_ps1((s->ylim[1] - s->ylim[0]) / s->height);
-//    mandelbrotState.threshold = _mm_set_ps1(4);
-//    mandelbrotState.one = _mm_set_ps1(1);
-//    mandelbrotState.iter_scale = _mm_set_ps1(1.0f / s->iterations);
-//    mandelbrotState.depth_scale = _mm_set_ps1(s->depth - 1);
-//
-//    mandelbrotState.depth_scale = _mm_mul_ps(mandelbrotState.xmin, mandelbrotState.ymin);
-
-//    for (int y = 0; y < s->height; y++) {
-//        for (int x = 0; x < s->width; x += 4) {
-//            mandelbrotState.mx = _mm_set_ps(x + 3, x + 2, x + 1, x + 0);
-//            mandelbrotState.my = _mm_set_ps1(y);
-//            mandelbrotState.cr = _mm_add_ps(_mm_mul_ps(mandelbrotState.mx, mandelbrotState.xscale), mandelbrotState.xmin);
-//            mandelbrotState.ci = _mm_add_ps(_mm_mul_ps(mandelbrotState.my, mandelbrotState.yscale), mandelbrotState.ymin);
-//            mandelbrotState.zr = mandelbrotState.cr;
-//            mandelbrotState.zi = mandelbrotState.ci;
-//            int k = 1;
-//            mandelbrotState.mk = _mm_set_ps1(k);
-//            while (++k < s->iterations) {
-//                /* Compute z1 from z0 */
-//                mandelbrotState.zr2 = _mm_mul_ps(mandelbrotState.zr, mandelbrotState.zr);
-//                mandelbrotState.zi2 = _mm_mul_ps(mandelbrotState.zi, mandelbrotState.zi);
-//                mandelbrotState.zrzi = _mm_mul_ps(mandelbrotState.zr, mandelbrotState.zi);
-//                mandelbrotState.zr = _mm_add_ps(_mm_sub_ps(mandelbrotState.zr2, mandelbrotState.zi2), mandelbrotState.cr);
-//                mandelbrotState.zi = _mm_add_ps(_mm_add_ps(mandelbrotState.zrzi, mandelbrotState.zrzi), mandelbrotState.ci);
-//
-//                /* Increment k */
-//                mandelbrotState.zr2 = _mm_mul_ps(mandelbrotState.zr, mandelbrotState.zr);
-//                mandelbrotState.zi2 = _mm_mul_ps(mandelbrotState.zi, mandelbrotState.zi);
-//                mandelbrotState.mag2 = _mm_add_ps(mandelbrotState.zr2, mandelbrotState.zi2);
-//                mandelbrotState.mask = _mm_cmplt_ps(mandelbrotState.mag2, mandelbrotState.threshold);
-//                mandelbrotState.mk = _mm_add_ps(_mm_and_ps(mandelbrotState.mask, mandelbrotState.one), mandelbrotState.mk);
-//
-//                /* Early bailout? */
-//                if (_mm_movemask_ps(mandelbrotState.mask) == 0)
-//                    break;
-//            }
-//            mandelbrotState.mk = _mm_mul_ps(mandelbrotState.mk, mandelbrotState.iter_scale);
-//            mandelbrotState.mk = _mm_sqrt_ps(mandelbrotState.mk);
-//            mandelbrotState.mk = _mm_mul_ps(mandelbrotState.mk, mandelbrotState.depth_scale);
-//            mandelbrotState.pixels = _mm_cvtps_epi32(mandelbrotState.mk);
-//            unsigned char *dst = image + y * s->width * 3 + x * 3;
-//            unsigned char *src = (unsigned char *)&mandelbrotState.pixels;
-//            for (int i = 0; i < 4; i++) {
-//                dst[i * 4 + 0] = 0x0;
-//                dst[i * 4 + 1] = src[i * 4];
-//                dst[i * 4 + 2] = src[i * 4];
-//                dst[i * 4 + 3] = src[i * 4];
-//            }
-//        }
-//    }
-}
-
-void Mandelbrot::mandel() {
-
-    struct spec spec = {
-            .width = 640,
-            .height = 480,
-            .depth = 256,
-            .xlim = {-2.5, 1.5},
-            .ylim = {-1.5, 1.5},
-            .iterations = 256
-    };
-
-    unsigned char *image = (unsigned char*) malloc(spec.width * spec.height * 4);
-
-    log.trace("Calculate start");
-
-    mandel_sse2(image, &spec);
-
-    log.trace("Calculate end");
-
-    uint32_t *iterator = (uint32_t*) image;
-
-    log.trace("Draw start");
-
-//    for (uint32_t i = 0; i < 640 * 480; i++) {
-//
-//        lfb->drawPixel(i % 640, i / 640, Color(iterator[i], 32));
-//    }
-
-    log.trace("Draw end");
-}
-
 Color Mandelbrot::colors[MAX_COLORS] {
-    Color(77, 157, 224),
-    Color(255, 85, 84),
-    Color(255, 188, 41),
-    Color(59, 178, 115),
-    Color(119, 104, 174)
+        Color(77, 157, 224),
+        Color(255, 85, 84),
+        Color(255, 188, 41),
+        Color(59, 178, 115),
+        Color(119, 104, 174)
 };
+
+__attribute__ ((aligned (16))) Mandelbrot::Properties Mandelbrot::properties;
+
+__attribute__ ((aligned (16))) Mandelbrot::State Mandelbrot::state;
+
+LinearFrameBuffer *Mandelbrot::lfb = nullptr;
 
 Mandelbrot::Mandelbrot() : Thread("Mandelbrot"), log(Logger::get("Mandelbrot")) {
 
 }
+
+__attribute__((force_align_arg_pointer)) void drawMandelbrot() {
+
+    Mandelbrot::state.xmin = _mm_set_ps1(Mandelbrot::properties.xlim[0]);
+    Mandelbrot::state.ymin = _mm_set_ps1(Mandelbrot::properties.ylim[0]);
+    Mandelbrot::state.xscale = _mm_set_ps1((Mandelbrot::properties.xlim[1] - Mandelbrot::properties.xlim[0]) / Mandelbrot::properties.width);
+    Mandelbrot::state.yscale = _mm_set_ps1((Mandelbrot::properties.ylim[1] - Mandelbrot::properties.ylim[0]) / Mandelbrot::properties.height);
+    Mandelbrot::state.threshold = _mm_set_ps1(4);
+    Mandelbrot::state.one = _mm_set_ps1(1);
+    Mandelbrot::state.iter_scale = _mm_set_ps1(1.0F / Mandelbrot::properties.iterations);
+    Mandelbrot::state.depth_scale = _mm_set_ps1(Mandelbrot::properties.depth - 1);
+
+    Mandelbrot::state.depth_scale = _mm_mul_ps(Mandelbrot::state.xmin, Mandelbrot::state.ymin);
+
+    for (uint32_t y = 0; y < Mandelbrot::properties.height; y++) {
+
+        for (uint32_t x = 0; x < Mandelbrot::properties.width; x += 4) {
+
+            Mandelbrot::state.mx = _mm_set_ps(x + 3, x + 2, x + 1, x + 0);
+            Mandelbrot::state.my = _mm_set_ps1(y);
+            Mandelbrot::state.cr = _mm_add_ps(_mm_mul_ps(Mandelbrot::state.mx, Mandelbrot::state.xscale), Mandelbrot::state.xmin);
+            Mandelbrot::state.ci = _mm_add_ps(_mm_mul_ps(Mandelbrot::state.my, Mandelbrot::state.yscale), Mandelbrot::state.ymin);
+            Mandelbrot::state.zr = Mandelbrot::state.cr;
+            Mandelbrot::state.zi = Mandelbrot::state.ci;
+
+            uint32_t k = 1;
+            Mandelbrot::state.mk = _mm_set_ps1(k);
+
+            while (++k < Mandelbrot::properties.iterations) {
+
+                Mandelbrot::state.zr2 = _mm_mul_ps(Mandelbrot::state.zr, Mandelbrot::state.zr);
+                Mandelbrot::state.zi2 = _mm_mul_ps(Mandelbrot::state.zi, Mandelbrot::state.zi);
+                Mandelbrot::state.zrzi = _mm_mul_ps(Mandelbrot::state.zr, Mandelbrot::state.zi);
+                Mandelbrot::state.zr = _mm_add_ps(_mm_sub_ps(Mandelbrot::state.zr2, Mandelbrot::state.zi2), Mandelbrot::state.cr);
+                Mandelbrot::state.zi = _mm_add_ps(_mm_add_ps(Mandelbrot::state.zrzi, Mandelbrot::state.zrzi), Mandelbrot::state.ci);
+
+                Mandelbrot::state.zr2 = _mm_mul_ps(Mandelbrot::state.zr, Mandelbrot::state.zr);
+                Mandelbrot::state.zi2 = _mm_mul_ps(Mandelbrot::state.zi, Mandelbrot::state.zi);
+                Mandelbrot::state.mag2 = _mm_add_ps(Mandelbrot::state.zr2, Mandelbrot::state.zi2);
+                Mandelbrot::state.mask = _mm_cmplt_ps(Mandelbrot::state.mag2, Mandelbrot::state.threshold);
+                Mandelbrot::state.mk = _mm_add_ps(_mm_and_ps(Mandelbrot::state.mask, Mandelbrot::state.one), Mandelbrot::state.mk);
+
+                if (_mm_movemask_ps(Mandelbrot::state.mask) == 0) {
+
+                    break;
+                }
+            }
+
+            Mandelbrot::state.mk = _mm_mul_ps(Mandelbrot::state.mk, Mandelbrot::state.iter_scale);
+            Mandelbrot::state.mk = _mm_sqrt_ps(Mandelbrot::state.mk);
+            Mandelbrot::state.mk = _mm_mul_ps(Mandelbrot::state.mk, Mandelbrot::state.depth_scale);
+            Mandelbrot::state.pixels = _mm_cvtps_epi32(Mandelbrot::state.mk);
+
+            unsigned char *src = (unsigned char *)&Mandelbrot::state.pixels;
+
+            for (int i = 0; i < 4; i++) {
+
+                Mandelbrot::lfb->drawPixel(x + i, y, Mandelbrot::colors[src[i * 4] % Mandelbrot::MAX_COLORS]);
+            }
+        }
+    }
+}
+
+
 
 void Mandelbrot::run() {
 
@@ -160,21 +95,26 @@ void Mandelbrot::run() {
 
     lfb->init(640, 480, 16);
 
-    mandel();
-
     lfb->drawPixel(0, 0, Colors::WHITE);
 
     Kernel::getService<EventBus>()->subscribe(*this, KeyEvent::TYPE);
 
+    uint16_t xRes = lfb->getResX();
+
+    uint16_t yRes = lfb->getResY();
+
+    realBase = 4.0 / xRes;
+
+    imaginaryBase = 4.0 / yRes;
+
+    log.trace("Draw start");
+
+    drawMandelbrot();
+
+    log.trace("Draw end");
+
     while(isRunning);
 
-//    uint16_t xRes = lfb->getResX();
-//
-//    uint16_t yRes = lfb->getResY();
-//
-//    realBase = 4.0 / xRes;
-//
-//    imaginaryBase = 4.0 / yRes;
 //
 //    while (isRunning) {
 //
--- src/os/apps/Mandelbrot/Mandelbrot.h
+++ src/os/apps/Mandelbrot/Mandelbrot.h
@@ -4,6 +4,7 @@
 #include <kernel/events/Receiver.h>
 #include <cstdint>
 #include <kernel/log/Logger.h>
+#include <xmmintrin.h>
 #include "kernel/threads/Thread.h"
 
 #include "apps/AntApp/Limits.h"
@@ -16,6 +17,39 @@
  * Mandelbrot.
  */
 class Mandelbrot : public Thread, public Receiver {
+
+    struct State {
+        __m128 xmin;
+        __m128 ymin;
+        __m128 xscale;
+        __m128 yscale;
+        __m128 threshold;
+        __m128 one;
+        __m128 iter_scale;
+        __m128 depth_scale;
+        __m128 mx;
+        __m128 my;
+        __m128 cr;
+        __m128 ci;
+        __m128 zr;
+        __m128 zi;
+        __m128 mk;
+        __m128 zr2;
+        __m128 zi2;
+        __m128 zrzi;
+        __m128 mag2;
+        __m128 mask;
+        __m128i pixels;
+    } __attribute__ ((aligned (16)));
+
+    struct Properties {
+        uint32_t width = 640;
+        uint32_t height = 480;
+        uint32_t depth = 256;
+        uint32_t iterations = 256;
+        float xlim[2] = {-2.1, 0.6F};
+        float ylim[2] = {-1.35F, 1.35F};
+    } __attribute__ ((aligned (16)));
 
 public:
 
@@ -31,23 +65,23 @@ public:
 
     void onEvent(const Event &event) override;
 
-private:
+    static State state;
 
-    Logger &log;
-
-    Color colorAt(uint32_t x, uint32_t y, double offsetX, double offsetY, double zoom);
-
-    LinearFrameBuffer *lfb = nullptr;
-
-    static constexpr const double START_REAL = 0.0;
-
-    static constexpr const double START_IMAGINARY = 0.0;
+    static Properties properties;
 
     static const uint32_t MAX_VALUE = 255;
 
     static const uint32_t MAX_COLORS = 5;
 
     static Color colors[MAX_COLORS];
+
+    static LinearFrameBuffer *lfb;
+
+private:
+
+    Logger &log;
+
+    Color colorAt(uint32_t x, uint32_t y, double offsetX, double offsetY, double zoom);
 
     double realBase = 0.0;
 
@@ -62,8 +96,6 @@ private:
     bool shouldDraw = true;
 
     bool isRunning = true;
-
-    void mandel();
 };
 
 #endif
