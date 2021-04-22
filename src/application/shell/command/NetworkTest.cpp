599d1733026f89f1550d04961d12e17e2778e189
feilsystem <hannes.feil@hhu.de>
2021-04-22 18:17:09 +0200

    Added device selection to existing NetworkTest CLI app. Our new Loopback device can be tested now!

--- src/application/shell/command/NetworkTest.cpp
+++ src/application/shell/command/NetworkTest.cpp
@@ -34,6 +34,7 @@ void NetworkTest::execute(Util::Array<String> &args) {
     parser.addParameter("send", "s", false);
     parser.addParameter("time", "t", false);
     parser.addParameter("count", "c", false);
+    parser.addParameter("index", "i",false);
 
     if (!parser.parse(args)) {
         stderr << args[0] << ": " << parser.getErrorString() << endl;
@@ -47,7 +48,18 @@ void NetworkTest::execute(Util::Array<String> &args) {
         return;
     }
 
-    NetworkDevice &driver = Kernel::System::getService<Kernel::NetworkService>()->getDriver(0);
+    uint8_t index = 0;
+
+    if(!parser.getNamedArgument("index").isEmpty()){
+        index = static_cast<uint8_t>(strtoint((char *) parser.getNamedArgument("index")));
+        if(index<0 || index >= UINT8_MAX || index >= networkService->getDeviceCount()){
+            stderr << "Invalid index value! Falling back to zero (default)" << endl;
+            index = 0;
+        }
+        printf("Device with index %d chosen\n",index);
+    }
+
+    NetworkDevice &driver = Kernel::System::getService<Kernel::NetworkService>()->getDriver(index);
 
     if(!parser.getNamedArgument("send").isEmpty()) {
         int length = static_cast<uint32_t>(strtoint((char *) parser.getNamedArgument("send")));
