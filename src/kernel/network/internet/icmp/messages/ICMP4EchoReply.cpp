82a4d5af8297d265a269a0f94102d0490c21eef8
feilsystem <hannes.feil@hhu.de>
2021-05-18 21:15:47 +0200

    Implemented methods of ICMP4EchoReply, similar to ICMP4Echo

--- src/kernel/network/internet/icmp/messages/ICMP4EchoReply.cpp
+++ src/kernel/network/internet/icmp/messages/ICMP4EchoReply.cpp
@@ -4,16 +4,35 @@
 
 #include "ICMP4EchoReply.h"
 
-ICMP4EchoReply::ICMP4EchoReply() {}
+ICMP4EchoReply::ICMP4EchoReply(uint16_t identifier, uint16_t sequenceNumber) {
+    myMessage.type = 0; //8 for echo, 0 for echo reply (RFC792)
+    myMessage.code = 0;
+    myMessage.checksum = 0;
+    myMessage.identifier = identifier;
+    myMessage.sequenceNumber = sequenceNumber;
+}
 
 ICMP4EchoReply::ICMP4EchoReply(IP4DataPart *dataPart) {
-    //TODO: Implement parsing from input
+    auto *input = static_cast<echoReplyMessage *>(dataPart->getMemoryAddress());
+    //TODO: Check for valid type
+    myMessage.code = input->code;
+    myMessage.checksum = input->checksum;
+    myMessage.identifier = input->identifier;
+    myMessage.sequenceNumber = input->sequenceNumber;
 }
 
 void *ICMP4EchoReply::getMemoryAddress() {
-    return nullptr;
+    return &myMessage;
 }
 
 uint16_t ICMP4EchoReply::getLength() {
-    return 0;
+    return sizeof(echoReplyMessage);
+}
+
+uint16_t ICMP4EchoReply::getIdentifier() {
+    return myMessage.identifier;
+}
+
+uint16_t ICMP4EchoReply::getSequenceNumber() {
+    return myMessage.sequenceNumber;
 }
--- src/kernel/network/internet/icmp/messages/ICMP4EchoReply.h
+++ src/kernel/network/internet/icmp/messages/ICMP4EchoReply.h
@@ -8,9 +8,19 @@
 #include <kernel/network/internet/icmp/ICMP4Message.h>
 
 class ICMP4EchoReply : public ICMP4Message {
+private:
+    typedef struct icmp4echoReply {
+        uint8_t type;
+        uint8_t code;
+        uint16_t checksum;
+        uint16_t identifier;
+        uint16_t sequenceNumber;
+    } echoReplyMessage;
+
+    echoReplyMessage myMessage;
 public:
     //Sending constructor
-    ICMP4EchoReply();
+    ICMP4EchoReply(uint16_t identifier, uint16_t sequenceNumber);
 
     //Receiving constructor
     ICMP4EchoReply(IP4DataPart *dataPart);
@@ -18,6 +28,10 @@ public:
     void *getMemoryAddress() override;
 
     uint16_t getLength() override;
+
+    uint16_t getIdentifier();
+
+    uint16_t getSequenceNumber();
 };
 
 
