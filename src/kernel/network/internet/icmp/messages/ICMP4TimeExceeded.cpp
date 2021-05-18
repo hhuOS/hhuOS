352ae06e94bb4b82f9d6e5b513823b08df096f35
feilsystem <hannes.feil@hhu.de>
2021-05-18 15:46:17 +0200

    Implemented ICMP4TimeExceeded message.

---
+++ src/kernel/network/internet/icmp/messages/ICMP4TimeExceeded.cpp
@@ -0,0 +1,19 @@
+//
+// Created by hannes on 18.05.21.
+//
+
+#include "ICMP4TimeExceeded.h"
+
+ICMP4TimeExceeded::ICMP4TimeExceeded() {}
+
+ICMP4TimeExceeded::ICMP4TimeExceeded(IP4DataPart *dataPart) {
+    //TODO: Implement parsing from input
+}
+
+void *ICMP4TimeExceeded::getMemoryAddress() {
+    return nullptr;
+}
+
+uint16_t ICMP4TimeExceeded::getLength() {
+    return 0;
+}
---
+++ src/kernel/network/internet/icmp/messages/ICMP4TimeExceeded.h
@@ -0,0 +1,24 @@
+//
+// Created by hannes on 18.05.21.
+//
+
+#ifndef HHUOS_ICMP4TIMEEXCEEDED_H
+#define HHUOS_ICMP4TIMEEXCEEDED_H
+
+
+#include <kernel/network/internet/icmp/ICMP4Message.h>
+
+class ICMP4TimeExceeded : public ICMP4Message{
+    //Sending constructor
+    ICMP4TimeExceeded();
+
+    //Receiveing constructor
+    ICMP4TimeExceeded(IP4DataPart *dataPart);
+
+    void *getMemoryAddress() override;
+
+    uint16_t getLength() override;
+};
+
+
+#endif //HHUOS_ICMP4TIMEEXCEEDED_H
