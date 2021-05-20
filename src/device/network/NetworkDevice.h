32c8df4ae994d17aa33da6ba81691cc573bf7860
feilsystem <hannes.feil@hhu.de>
2021-05-20 21:55:30 +0200

    Added IP4Address and IP4Netmask to Interface NetworkDevice, together with getter and setter methods. We now can set/get IP4Addresses/Netmasks from interfaces without changing existing code.

--- src/device/network/NetworkDevice.h
+++ src/device/network/NetworkDevice.h
@@ -19,6 +19,8 @@
 
 #include "kernel/interrupt/InterruptHandler.h"
 #include <cstdint>
+#include <kernel/network/internet/IP4Address.h>
+#include <kernel/network/internet/IP4Netmask.h>
 
 /**
  * Interface for network cards
@@ -27,6 +29,9 @@
  * @date 2019
  */
 class NetworkDevice : public Kernel::InterruptHandler {
+private:
+    IP4Address *ip4Address = nullptr;
+    IP4Netmask *ip4Netmask = nullptr;
 
 public:
 
@@ -39,6 +44,22 @@ public:
      * Copy-constructor.
      */
     NetworkDevice(const NetworkDevice &copy) = delete;
+
+    IP4Address *getIp4Address() const {
+        return ip4Address;
+    }
+
+    void setIp4Address(IP4Address *ip4Address) {
+        NetworkDevice::ip4Address = ip4Address;
+    }
+
+    IP4Netmask *getIp4Netmask() const {
+        return ip4Netmask;
+    }
+
+    void setIp4Netmask(IP4Netmask *ip4Netmask) {
+        NetworkDevice::ip4Netmask = ip4Netmask;
+    }
 
     /**
      * Assignment operator.
