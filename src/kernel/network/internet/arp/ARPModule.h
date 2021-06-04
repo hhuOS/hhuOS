a52fe18a83442a6e73519d173f21c5aa78b17f3a
feilsystem <hannes.feil@hhu.de>
2021-06-04 22:49:55 +0200

    SNAPSHOT: Restored ARPModule...

---
+++ src/kernel/network/internet/arp/ARPModule.h
@@ -0,0 +1,33 @@
+//
+// Created by hannes on 15.05.21.
+//
+
+#ifndef HHUOS_ARPMODULE_H
+#define HHUOS_ARPMODULE_H
+
+#include <lib/util/HashMap.h>
+#include <kernel/network/internet/addressing/IP4Address.h>
+#include <kernel/network/ethernet/EthernetAddress.h>
+#include <kernel/network/ethernet/EthernetFrame.h>
+#include <kernel/network/internet/IP4Datagram.h>
+#include "ARPEntry.h"
+
+class ARPModule {
+private:
+    //HashMap did not work here, possible bug in HashMap implementation?
+    Util::ArrayList<ARPEntry *> *arpTable;
+    EthernetAddress *broadcastAddress;
+
+    EthernetAddress *resolveIP4(IP4Address *ip4Address);
+
+public:
+
+    ARPModule();
+
+    void addEntry(IP4Address *ip4Address, EthernetAddress *ethernetAddress);
+
+    EthernetFrame *initEthernetFrame(IP4Address *ip4address, IP4Datagram *ip4datagram);
+};
+
+
+#endif //HHUOS_ARPMODULE_H
