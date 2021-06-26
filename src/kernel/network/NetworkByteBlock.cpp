4dc0b31b874cecb24542c9d99ebfb68fbd3af1d2
feilsystem <hannes.feil@hhu.de>
2021-06-27 00:08:53 +0200

    Added newlines all 20 bytes to NetworkByteBlock printout

--- src/kernel/network/NetworkByteBlock.cpp
+++ src/kernel/network/NetworkByteBlock.cpp
@@ -132,6 +132,11 @@ namespace Kernel {
             return byteString;
         }
         for (size_t i = startIndex; (i < length) && (i < endIndex); i++) {
+            if(((i-startIndex) % 20) == 0){
+                if((i-startIndex)>0) {
+                    byteString += "\n";
+                }
+            }
             byteString+=String::format("%02x ", bytes[i]);
         }
         return byteString;
