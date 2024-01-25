2839af3457297cef7b23573a9d72c7008984bccd
SandroMiudo <sandromiudo0@gmail.com>
2024-01-25 10:53:19 +0100

    registration functions to call the desired service routine in the usb service

---
+++ src/kernel/usb/UsbRegistry.h
@@ -0,0 +1,23 @@
+#include <cstdint>
+
+extern "C"{
+#include "../../device/usb/driver/UsbDriver.h"
+#include "../../device/usb/events/listeners/EventListener.h"
+#include "../../device/usb/dev/data/UsbDev_Data.h"
+}
+
+// define abstraction layer to call specific service routine (usb)!
+int interface_register_callback(uint16_t register_type, event_callback event_c);
+int interface_deregister_callback(uint16_t register_type, event_callback event_c);
+
+// :for kernel
+int interface_register_driver(UsbDriver* driver);
+int interface_deregister_driver(UsbDriver* driver);
+
+int interface_register_listener(EventListener* listener);
+int interface_deregister_listener(int id);
+
+void interface_submit_bulk_transfer(Interface* interface, unsigned int pipe, uint8_t prio, void* data, unsigned int len, callback_function callback);
+void interface_submit_interrupt_transfer(Interface* interface, unsigned int pipe, uint8_t prio, uint16_t interval, void* data, unsigned int len, callback_function callback);
+void interface_submit_control_transfer(Interface* interface, unsigned int pipe, uint8_t prio, void* data, uint8_t* setup, callback_function callback);
+//void interface_submit_iso_transfer(Interface* interface, unsigned int pipe, uint8_t prio);
