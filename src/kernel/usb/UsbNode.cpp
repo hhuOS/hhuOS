8c6d3b2a75a4c84df16c9f66ea098ae2a1b1e747
SandroMiudo <sandromiudo0@gmail.com>
2024-02-10 22:41:31 +0100

    each UsbNode does have its own minor number corresponding to an entry in the corresponding driver

--- src/kernel/usb/UsbNode.cpp
+++ src/kernel/usb/UsbNode.cpp
@@ -2,10 +2,15 @@
 #include "../../lib/util/io/stream/QueueInputStream.h"
 #include "../../lib/util/collection/ArrayBlockingQueue.h"
 #include "../../lib/util/io/stream/FilterInputStream.h"
+#include <cstdint>
 
 
-Kernel::Usb::UsbNode::UsbNode(void (*r_callback)(void *e)) : callback(r_callback) {}
+Kernel::Usb::UsbNode::UsbNode(void (*r_callback)(void *e), uint8_t minor) : callback(r_callback), minor(minor){}
 
 void (*Kernel::Usb::UsbNode::get_callback())(void* e) { 
     return this->callback;
+}
+
+uint8_t Kernel::Usb::UsbNode::get_minor(){
+    return this->minor;
 }
--- src/kernel/usb/UsbNode.h
+++ src/kernel/usb/UsbNode.h
@@ -2,6 +2,7 @@
 #define USB_NODE__INCLUDE
 
 #include <cstdint>
+#include "../../lib/util/base/String.h"
 
 namespace Kernel::Usb {
 
@@ -9,16 +10,20 @@ class UsbNode {
 
 public:
 
-    explicit UsbNode(void (*r_callback)(void* e));
+    explicit UsbNode(void (*r_callback)(void* e), uint8_t minor);
 
-    virtual int add_file_node() = 0;
+    virtual int add_file_node(Util::String node_name) = 0;
 
     ~UsbNode() = default;
 
     void (*get_callback())(void* e);
 
+    uint8_t get_minor();
+
 private:
     void (*callback)(void* e);
+
+    uint8_t minor;
 };
 
 };
