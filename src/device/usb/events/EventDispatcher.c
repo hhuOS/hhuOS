9242ea14834e2002aca9952b2a0f52296167d8fd
SandroMiudo <sandromiudo0@gmail.com>
2024-05-13 23:11:15 +0200

    Event Dispatcher component revision/refactoring

--- src/device/usb/events/EventDispatcher.c
+++ src/device/usb/events/EventDispatcher.c
@@ -4,86 +4,77 @@
 #include "event/Event.h"
 #include "listeners/EventListener.h"
 
-void publish_event(EventDispatcher *dispatcher, GenericEvent *event, int id) {
-  EventListener *event_listener = dispatcher->getListener(dispatcher, id);
+static void init_event_dispatcher_map(EventDispatcher* event_dispatcher);
+static void publish_event(EventDispatcher* dispatcher,  GenericEvent* event, int id); 
+static int register_event_listener(EventDispatcher* dispatcher, EventListener* event_listener);
+static int deregister_event_listener(EventDispatcher* dispatcher, int id);
+static EventListener* getListener(EventDispatcher* dispatcher, int id);
+static int reg_callback(EventDispatcher* dispatcher, event_callback callback, uint16_t event_listener_type);
+static int dereg_callback(EventDispatcher* dispatcher, event_callback callback, uint16_t event_listener_type);
 
-  if (event_listener == (void *)0)
-    return;
-
-  event_listener->call(event_listener,
-                       event); // call callbacks registered with event_listener
+static void publish_event(EventDispatcher *dispatcher, GenericEvent *event, int id) {
+  EventListener *event_listener = __STRUCT_CALL__(dispatcher, getListener, id);
+  __IF_RET__(__IS_NULL__(event_listener));
+  __STRUCT_CALL__(event_listener, call, event);
+    // call callbacks registered within event_listener
 }
 
-int register_event_listener(EventDispatcher *dispatcher,
+static int register_event_listener(EventDispatcher *dispatcher,
                             EventListener *event_listener) {
   int count = dispatcher->event_listener_counter;
-  dispatcher->event_listener_map->put_c(dispatcher->event_listener_map, &count,
-                                        event_listener);
+  __MAP_PUT__(dispatcher->event_listener_map, &count, event_listener);
   dispatcher->event_listener_counter = count + 1;
 
   // dispatcher->dispatcher_mutex->acquire_c(dispatcher->dispatcher_mutex);
 
-  list_element *l_e = dispatcher->head.l_e;
-  if (l_e == (void *)0) {
-    dispatcher->head.l_e = &event_listener->l_e;
+  list_element *l_e = __LIST_FIRST_ENTRY__(dispatcher->head);
+  if (__IS_NULL__(l_e)) {
+    __LIST_ADD_FIRST_ENTRY__(dispatcher->head, &event_listener->l_e);
     // dispatcher->dispatcher_mutex->release_c(dispatcher->dispatcher_mutex);
     return count;
   }
-  while (l_e->l_e != (void *)0) {
-    l_e = l_e->l_e;
-  }
+  __LIST_TRAVERSE__(l_e);
 
-  l_e->l_e = &event_listener->l_e;
+  __LIST_NEXT_ENTRY__(l_e, &event_listener->l_e);
 
   // dispatcher->dispatcher_mutex->release_c(dispatcher->dispatcher_mutex);
-
   return count;
 }
-int deregister_event_listener(EventDispatcher *dispatcher, int id) {
-  list_element *l_e = dispatcher->head.l_e;
+static int deregister_event_listener(EventDispatcher *dispatcher, int id) {
+  list_element *l_e = __LIST_FIRST_ENTRY__(dispatcher->head);
   list_element *prev = 0;
 
   // dispatcher->dispatcher_mutex->acquire_c(dispatcher->dispatcher_mutex);
 
   int found = 0;
-  while (l_e != (void *)0) {
+  while (__NOT_NULL__(l_e)) {
     EventListener *event_listener =
         (EventListener *)container_of(l_e, EventListener, l_e);
     if (event_listener->listener_id == id) {
       found = 1;
-      if (prev == (void *)0) {
-        dispatcher->head.l_e = l_e->l_e;
+      if (__IS_NULL__(prev)) {
+        __LIST_ADD_FIRST_ENTRY__(dispatcher->head, l_e->l_e);
       } else {
-        prev->l_e = l_e->l_e;
+        __LIST_NEXT_ENTRY__(prev, l_e->l_e);
       }
     }
   }
-
   // dispatcher->dispatcher_mutex->release_c(dispatcher->dispatcher_mutex);
+  __IF_CUSTOM__(found, __MAP_REMOVE__(dispatcher->event_listener_map, void*,
+    &id); return id);
 
-  if (found) {
-    dispatcher->event_listener_map->remove_c(dispatcher->event_listener_map,
-                                             &id);
-    return id;
-  }
-
-  return -1;
+  return __RET_E__;
 }
 
-EventListener *getListener(EventDispatcher *dispatcher, int id) {
-  if (dispatcher->event_listener_map->contains_c(dispatcher->event_listener_map,
-                                                 &id)) {
-    return (EventListener *)dispatcher->event_listener_map->get_c(
-        dispatcher->event_listener_map, &id);
-  }
-  return (void *)0;
+static EventListener *getListener(EventDispatcher *dispatcher, int id) {
+  return __MAP_GET__(dispatcher->event_listener_map, EventListener*, &id);
 }
 
-int reg_callback(EventDispatcher *dispatcher, event_callback callback,
+static int reg_callback(EventDispatcher *dispatcher, event_callback callback,
                  uint16_t event_listener_type) {
   // dispatcher->dispatcher_mutex->acquire_c(dispatcher->dispatcher_mutex);
 
-  list_element *l_e = dispatcher->head.l_e;
+  list_element *l_e = __LIST_FIRST_ENTRY__(dispatcher->head);
 
   while (l_e != (void *)0) {
     EventListener *listener =
@@ -101,7 +92,7 @@ int reg_callback(EventDispatcher *dispatcher, event_callback callback,
   return -1;
 }
 
-int dereg_callback(EventDispatcher *dispatcher, event_callback callback,
+static int dereg_callback(EventDispatcher *dispatcher, event_callback callback,
                    uint16_t event_listener_type) {
   // dispatcher->dispatcher_mutex->acquire_c(dispatcher->dispatcher_mutex);
 
@@ -120,29 +111,21 @@ int dereg_callback(EventDispatcher *dispatcher, event_callback callback,
 
   // dispatcher->dispatcher_mutex->release_c(dispatcher->dispatcher_mutex);
 
-  return -1;
+  return __RET_E__;
 }
 
 void new_event_dispatcher(EventDispatcher *event_dispatcher) {
-  event_dispatcher->publish_event = &publish_event;
-  event_dispatcher->register_event_listener = &register_event_listener;
-  event_dispatcher->deregister_event_listener = &deregister_event_listener;
-  event_dispatcher->getListener = &getListener;
-  event_dispatcher->reg_callback = &reg_callback;
-  event_dispatcher->dereg_callback = dereg_callback;
-  event_dispatcher->init_event_dispatcher_map = &init_event_dispatcher_map;
   event_dispatcher->event_listener_map = 0;
   event_dispatcher->event_listener_counter = 0;
   event_dispatcher->head.l_e = 0;
 
   event_dispatcher->dispatcher_mutex =
       (Mutex_C *)interface_allocateMemory(sizeof(Mutex_C), 0);
-  event_dispatcher->dispatcher_mutex->new_mutex = &new_mutex;
-  event_dispatcher->dispatcher_mutex->new_mutex(
-      event_dispatcher->dispatcher_mutex);
+  __STRUCT_INIT__(event_dispatcher->dispatcher_mutex, new_mutex, new_mutex);
+  __INIT_EVENT_DISPATCHER(event_dispatcher);
 }
 
-void init_event_dispatcher_map(EventDispatcher *event_dispatcher) {
+static void init_event_dispatcher_map(EventDispatcher *event_dispatcher) {
   EventMap *map = (EventMap *)interface_allocateMemory(sizeof(EventMap), 0);
   map->new_map = &newEventMap;
   map->new_map(map, "Map<int,EventListener*>");
--- src/device/usb/events/EventDispatcher.h
+++ src/device/usb/events/EventDispatcher.h
@@ -7,15 +7,22 @@
 #include "event/Event.h"
 #include "../interfaces/MutexInterface.h"
 
+#define __INIT_EVENT_DISPATCHER(name) \
+    __ENTRY__(name, publish_event) = &publish_event; \
+    __ENTRY__(name, register_event_listener) = &register_event_listener; \
+    __ENTRY__(name, deregister_event_listener) = &deregister_event_listener; \
+    __ENTRY__(name, getListener) = &getListener; \
+    __ENTRY__(name, reg_callback) = &reg_callback; \
+    __ENTRY__(name, dereg_callback) = &dereg_callback; \
+    __ENTRY__(name, init_event_dispatcher_map) = &init_event_dispatcher_map
+
 struct EventDispatcher{
     void (*publish_event)(struct EventDispatcher* dispatcher, GenericEvent* event, int id);
     int (*register_event_listener)(struct EventDispatcher* dispatcher, EventListener* event_listener);
     int (*deregister_event_listener)(struct EventDispatcher* dispatcher, int id);
     EventListener* (*getListener)(struct EventDispatcher* dispatcher, int id);
-
     int (*reg_callback)(struct EventDispatcher* dispatcher, event_callback callback, uint16_t event_listener_type);
     int (*dereg_callback)(struct EventDispatcher* dispatcher, event_callback callback, uint16_t event_listener_type);
-
     void (*new_event_dispatcher)(struct EventDispatcher* event_dispatcher);
     void (*init_event_dispatcher_map)(struct EventDispatcher* event_dispatcher);
 
@@ -28,16 +35,5 @@ struct EventDispatcher{
 typedef struct EventDispatcher EventDispatcher;
 
 void new_event_dispatcher(EventDispatcher* event_dispatcher);
-void init_event_dispatcher_map(EventDispatcher* event_dispatcher);
-
-void publish_event(EventDispatcher* dispatcher,  GenericEvent* event, int id); 
-
-int register_event_listener(EventDispatcher* dispatcher, EventListener* event_listener);
-int deregister_event_listener(EventDispatcher* dispatcher, int id);
-
-EventListener* getListener(EventDispatcher* dispatcher, int id);
-
-int reg_callback(EventDispatcher* dispatcher, event_callback callback, uint16_t event_listener_type);
-int dereg_callback(EventDispatcher* dispatcher, event_callback callback, uint16_t event_listener_type);
 
 #endif
