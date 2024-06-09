d9d65e486e185be4cbc67be44843154558b87424
SandroMiudo <sandromiudo0@gmail.com>
2024-06-09 20:37:27 +0200

    included usb nodes, 1 : stream 2 : memory

---
+++ src/kernel/usb/UsbMemoryNode.h
@@ -0,0 +1,17 @@
+#ifndef USB_MEM_NODE__INCLUDE
+#define USB_MEM_NODE__INCLUDE
+
+#include "UsbNode.h"
+#include "../../filesystem/memory/MemoryNode.h"
+
+namespace Kernel::Usb {
+
+class UsbMemoryNode : public UsbNode, Filesystem::Memory::MemoryNode {
+
+public:
+    UsbMemoryNode(const Util::String& name, uint8_t minor);
+};
+
+};
+
+#endif
---
+++ src/kernel/usb/UsbStreamNode.h
@@ -0,0 +1,25 @@
+#ifndef USB_STREAM_NODE__INCLUDE
+#define USB_STREAM_NODE__INCLUDE
+
+#include "UsbNode.h"
+#include "../../filesystem/memory/StreamNode.h"
+
+#include "../../lib/util/base/String.h"
+#include "../../lib/util/io/stream/InputStream.h"
+#include "../../lib/util/io/stream/OutputStream.h"
+
+namespace Kernel::Usb{
+
+class UsbStreamNode : public UsbNode, Filesystem::Memory::StreamNode{
+
+public: 
+    UsbStreamNode(const Util::String& name, Util::Io::InputStream* input_stream, uint8_t minor);
+    UsbStreamNode(const Util::String& name, Util::Io::OutputStream* output_stream, uint8_t minor);
+    UsbStreamNode(const Util::String& name, Util::Io::InputStream* input_stream, 
+        Util::Io::OutputStream* output_stream, uint8_t minor);
+
+};
+
+};
+
+#endif
