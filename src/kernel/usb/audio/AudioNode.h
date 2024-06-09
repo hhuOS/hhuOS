0616d8677dd28647364a16b661a9a4194799ee21
SandroMiudo <sandromiudo0@gmail.com>
2024-06-09 20:42:54 +0200

    Implementation of Audio Node : support for control io is also included

--- src/kernel/usb/audio/AudioNode.h
+++ src/kernel/usb/audio/AudioNode.h
@@ -1,6 +1,35 @@
 #ifndef AUDIO_NODE__INCLUDE
 #define AUDIO_NODE__INCLUDE
 
+#include "../UsbStreamNode.h"
+#include "../driver/KernelAudioDriver.h"
 
+namespace Kernel::Usb::Driver{
+  class KernelAudioDriver;
+};
+
+namespace Kernel::Usb{
+
+class AudioNode : public UsbStreamNode {
+
+public:
+    AudioNode(const Util::String& s, Driver::KernelAudioDriver* driver,
+        Util::Io::InputStream* input_stream, uint8_t minor);
+    
+    AudioNode(const Util::String& s, Driver::KernelAudioDriver* driver,
+        Util::Io::OutputStream* output_stream, uint8_t minor);
+
+    AudioNode(const Util::String& s, Driver::KernelAudioDriver* driver,
+        Util::Io::InputStream* input_stream, Util::Io::OutputStream* output_stream, 
+        uint8_t minor); 
+
+    int add_file_node() override;
+    bool control(uint32_t request, const Util::Array<uint32_t>& parameters) override;
+
+private:
+    Driver::KernelAudioDriver* audio_driver;
+};
+
+};
 
 #endif
