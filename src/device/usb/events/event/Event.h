9586ee40c6c959eedf57998a66294f1e526834b5
SandroMiudo <sandromiudo0@gmail.com>
2024-06-09 20:21:59 +0200

    1 - GenericEvent : field event_code = uint32_t -> raw data or actual input event 2 - changed signature, to contain the recovery buffer

--- src/device/usb/events/event/Event.h
+++ src/device/usb/events/event/Event.h
@@ -6,11 +6,11 @@
 struct GenericEvent{
     uint16_t event_value; // pressed, released or else
     uint16_t event_type; // mouse, kbd or else
-    uint16_t event_code; 
+    uint32_t event_code; // raw data or input event
 };
 
 typedef struct GenericEvent GenericEvent;
 
-typedef void (*event_callback)(void* event);
+typedef void (*event_callback)(void* event, void* buffer);
 
 #endif
