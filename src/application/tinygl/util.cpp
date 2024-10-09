40e76c1865f49aa77f636115ed1f418fbe057633
Fabian Ruhland <ruhland@hhu.de>
2024-10-09 17:00:33 +0200

    application/tinygl: Implement tutorial lesson1

---
+++ src/application/tinygl/util.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2018-2024 Heinrich-Heine-Universitaet Duesseldorf,
+ * Institute of Computer Science, Department Operating Systems
+ * Burak Akguel, Christian Gesse, Fabian Ruhland, Filip Krakowski, Michael Schoettner
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include "lib/util/math/Math.h"
+#include "util.h"
+
+/**
+ * Taken from https://stackoverflow.com/questions/12943164/replacement-for-gluperspective-with-glfrustrum
+ */
+void glPerspective(GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar) {
+    const GLdouble pi = Util::Math::PI;
+    GLdouble fW, fH;
+
+    fH = Util::Math::tangent(fovY / 360 * pi) * zNear;
+    fW = fH * aspect;
+
+    glFrustum(-fW, fW, -fH, fH, zNear, zFar);
+}
---
+++ src/application/tinygl/util.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2018-2024 Heinrich-Heine-Universitaet Duesseldorf,
+ * Institute of Computer Science, Department Operating Systems
+ * Burak Akguel, Christian Gesse, Fabian Ruhland, Filip Krakowski, Michael Schoettner
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#ifndef HHUOS_TINYGL_H
+#define HHUOS_TINYGL_H
+
+#include <GL/gl.h>
+
+/**
+ * Taken from https://stackoverflow.com/questions/12943164/replacement-for-gluperspective-with-glfrustrum
+ */
+void glPerspective(GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar);
+
+#endif
