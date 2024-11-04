9a800d7997b48ef4343f05606539575932be84b2
Fabian Ruhland <ruhland@hhu.de>
2024-11-04 15:40:39 +0100

    lib: Fix Util::Math::max()

--- src/lib/util/math/Math.cpp
+++ src/lib/util/math/Math.cpp
@@ -60,7 +60,7 @@ float min(float first, float second, float third) {
 }
 
 float max(float first, float second, float third) {
-    return max(min(first, second), third);
+    return max(max(first, second), third);
 }
 
 double min(double first, double second, double third) {
@@ -68,7 +68,7 @@ double min(double first, double second, double third) {
 }
 
 double max(double first, double second, double third) {
-    return max(min(first, second), third);
+    return max(max(first, second), third);
 }
 
 float exp(float arg) {
